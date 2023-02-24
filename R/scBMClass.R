6c25510138940aafc50b8bd0138200011a39a1e7
Maximilian Knoll <m.knoll@dkfz.de>
2023-02-24 13:48:00 +0100

    changed to R6

--- R/scBMClass.R
+++ R/scBMClass.R
@@ -1,29 +1,15 @@
-# S4 Helper classes
-setClassUnion("numericOrNULL", c("numeric", "NULL"))    
-setClassUnion("dfOrNULL", c("data.frame", "NULL"))    
-setClassUnion("listOrNULL", c("list", "NULL"))    
-setClassUnion("characterOrNULL", c("character", "NULL"))
+scBM <- R6Class("scBM",
+		public=list(
+			    seuratObject=NULL,
+			    initialize=function(
+						seuratObject=NA) {
+				self$seuratObject <- seuratObject 
+				self$greet()
+			    },
+			    greet=function() {
+				print("Hi there")
+			    }
+		)
+	)
 
-#' @title A S4 class representing a scBM instance
-#'
-#' @name scBM-class
-#' @exportClass scBM
-scBM <- setClass("scBM", 
-		 slots=c(dateCreated="POSIXct"
-			 ))
 
-#' @title Constructor scBM class
-#' 
-#' @description Constructor for scBM object
-#'
-#' @param .Object class instance
-#' @param dateCreated Date of creation
-#' @param seuratObject seurat object 
-setMethod("initilaize", "scBM", 
-	  function(.Object, 
-		   dateCreated=base::POSICxt,
-		   ...) {
-	      .Object@dateCreated=Sys.time()
-
-	      .Object
-	  })
