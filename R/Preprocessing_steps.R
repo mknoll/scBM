019901d0d8be59f7da89126b529170362380cf9d
Priyanka060 <72569649+Priyanka060@users.noreply.github.com>
2023-03-06 06:17:11 +0100

    Update Preprocessing_steps.R

--- R/Preprocessing_steps.R
+++ R/Preprocessing_steps.R
@@ -7,59 +7,49 @@ library(ggplot2)
 library(readr)
 library(tidyverse)
 
-#Loading dataset
-E_MTAB_9435 <- ReadMtx(mtx = "C:/Users/dhari/Documents/GBM/E-MTAB-9435.aggregated_filtered_counts.mtx",
-                       cells = "C:/Users/dhari/Documents/GBM/E-MTAB-9435.aggregated_filtered_counts.mtx_cols",
-                       features = "C:/Users/dhari/Documents/GBM/E-MTAB-9435.aggregated_filtered_counts.mtx_rows" )
 
-#Raw preprocessing based on sum of cell counts
-library(scran)
-library(scuttle)
-
-qcstats <- perCellQCMetrics(E_MTAB_9435)
-qcstats
-qcfiler <- quickPerCellQC(qcstats)
-qcfiler
-GBM_a <- E_MTAB_9435[,!qcfiler$discard]
-summary(qcfiler$discard)
-
-#Creating seurat object
-GBM_1 <- CreateSeuratObject(counts = GBM_a, project = "Glioblastoma", min.features = 200, min.cells = 5)
-GBM_1
+t1 <- readRDS("/filename.rds")
 
 #Preprocessing and visualization 
-GBM_1[["percent.mt"]] <- PercentageFeatureSet(GBM_1, pattern = "^MT-")
-GBM_1[["log10Genes.per.UMI"]] <- log10(GBM_1$nFeature_RNA) / log10(GBM_1$nCount_RNA)
-tail(GBM_1@meta.data, 5)
+t1[["percent.mt"]] <- PercentageFeatureSet(t1, pattern = "^MT-")
+t1[["log10Genes.per.UMI"]] <- log10(t1$nFeature_RNA) / log10(t1$nCount_RNA)
+VlnPlot(t1, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
-VlnPlot(GBM_1, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-nCount_max <- quantile(GBM_1$nCount_RNA, probs = 0.95)
-nFeature_max <- quantile(GBM_1$nFeature_RNA, probs = 0.95)
+t1 <- c(t1)
+preprocessing = function(x) {
+  median_ncount <- median(x$nCount_RNA)
+  median_nfeatures <- median(x$nFeature_RNA)
+  median_mt <- median(x$percent.mt)
+  
+  mad_ncounts <- mad(x$nCount_RNA, center = median_ncount)
+  mad_nfeatures <- mad(x$nFeature_RNA, center = median_nfeatures)
+  mad_mean_mt <- mad(x$percent.mt, center = median_mt)
+  
+  total_ncounts <- median_ncount+mad_ncounts
+  total_nfeatures <- median_nfeatures+mad_nfeatures
+  total_mt <- median_mt+mad_mean_mt
+  
+  x@meta.data %>% 
+    ggplot(aes(x=nCount_RNA, y=nFeature_RNA, color=percent.mt)) + 
+    geom_point() + 
+    scale_colour_gradient(low = "gray90", high = "black") + 
+    scale_x_log10() + 
+    scale_y_log10() + 
+    theme_classic() +
+    geom_vline(xintercept = total_ncounts) +
+    geom_hline(yintercept = total_nfeatures) +
+    geom_vline(xintercept = 500) +
+    geom_hline(yintercept = 250) +
+    facet_wrap(~orig.ident)
+  
+  x<- subset(x, subset = (nFeature_RNA >= 250) & 
+                  (nFeature_RNA <= total_nfeatures) &
+                  (nCount_RNA >= 500) & 
+                  (nCount_RNA <= total_ncounts) &
+                  (percent.mt < total_mt) &
+                  (log10Genes.per.UMI > 0.8))
+ 
+}
 
-# Visualize the correlation between genes detected and number of UMIs and determine whether strong presence of cells with low numbers of genes/UMIs
-GBM_1@meta.data %>% 
-  ggplot(aes(x=nCount_RNA, y=nFeature_RNA, color=percent.mt)) + 
-  geom_point() + 
-  scale_colour_gradient(low = "gray90", high = "black") + 
-  scale_x_log10() + 
-  scale_y_log10() + 
-  theme_classic() +
-  geom_vline(xintercept = nCount_max) +
-  geom_hline(yintercept = nFeature_max) +
-  geom_vline(xintercept = 500) +
-  geom_hline(yintercept = 250) +
-  facet_wrap(~orig.ident)
-
-#Featureplot
-plot1 <- FeatureScatter(GBM_1, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(GBM_1, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-plot1 + plot2
-
-#Filtering of cells based on desired parameters- Cell-level filtering.
-
-GBM_1<- subset(GBM_1, subset = (nFeature_RNA >= 250) & 
-                 (nFeature_RNA <= nFeature_max) &
-                 (nCount_RNA >= 500) & 
-                 (nCount_RNA <= nCount_max) &
-                 (percent.mt < 10) &
-                 (log10Genes.per.UMI > 0.8))
+preprocessed.obj <- lapply(t1, preprocessing)
+preprocessed.obj
